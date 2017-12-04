5c47d368678b943b11efd7d3aaf5a7bc164c3f37
Nanda Sundaresan <sundaresan.nanda@gmail.com>
2017-12-03 19:43:36 -0800

    clean formatting

--- scripts/spd_data.R
+++ scripts/spd_data.R
@@ -6,7 +6,8 @@ setwd('/Users/nsundaresan/Google Drive/College/Autumn 2017/INFO 201/Final-Projec
 source('scripts/api_keys.R')
 
 GetSPDData <- function() {
-  response <- GET("https://data.seattle.gov/resource/y7pv-r3kh.json?$limit=5000&$order=date_reported%20DESC", add_headers("X-API-Key" = seattle.app.token))
+  response <- GET("https://data.seattle.gov/resource/y7pv-r3kh.json?$limit=5000&$order=date_reported%20DESC", 
+                  add_headers("X-API-Key" = seattle.app.token))
   body <- content(response, "text")
   data <- fromJSON(body)
   final.data <- data %>% 
