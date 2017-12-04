d1d7f0d2f51fa07d111eb72c960644bd6684a7bd
Nanda Sundaresan <sundaresan.nanda@gmail.com>
2017-12-03 19:42:44 -0800

    filtered out irrelevant columns, close #4

--- scripts/spd_data.R
+++ scripts/spd_data.R
@@ -1,5 +1,6 @@
 library(httr)
 library(jsonlite)
+library(dplyr)
 
 setwd('/Users/nsundaresan/Google Drive/College/Autumn 2017/INFO 201/Final-Project-AB3')
 source('scripts/api_keys.R')
@@ -8,5 +9,9 @@ GetSPDData <- function() {
   response <- GET("https://data.seattle.gov/resource/y7pv-r3kh.json?$limit=5000&$order=date_reported%20DESC", add_headers("X-API-Key" = seattle.app.token))
   body <- content(response, "text")
   data <- fromJSON(body)
-  return(data)
+  final.data <- data %>% 
+    select(date_reported, latitude, longitude, summarized_offense_description)
+  return(final.data)
 }
+
+thing2 <- GetSPDData()
