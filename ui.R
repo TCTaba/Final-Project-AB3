2d6481996be9d4a06a26aaece116c09342027ebf
Rebecca Liu <rebeccaliu@D-108-179-178-132.dhcp4.washington.edu>
2017-12-03 19:20:28 -0800

    ui file started

---
+++ ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+
+my.ui <- navbarPage(
+  
+  # Application title
+  "Test Shiny",
+  
+  # Sidebar with a slider input for number of bins 
+  tabPanel("Overview",
+           sidebarLayout(
+             sidebarPanel(
+             ),
+             
+             # Show a plot of the generated distribution
+             mainPanel(
+              # plotlyOutput("distPlot")
+             )
+           )
+  ),
+  tabPanel("Histogram",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput(
+               )
+               )
+             ),
+             
+             # Show a plot of the generated distribution
+             mainPanel(
+               plotlyOutput("histPlot")
+             )
+           )
+  )
+)
+
+shinyUI(my.ui)
