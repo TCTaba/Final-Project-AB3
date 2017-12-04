43522acd78b30db2b57de48dd216d63696609fcd
Nanda Sundaresan <sundaresan.nanda@gmail.com>
2017-12-03 19:28:00 -0800

    modified to make big function, close #2

--- scripts/spd_data.R
+++ scripts/spd_data.R
@@ -1,11 +1,12 @@
 library(httr)
 library(jsonlite)
-library(dplyr)
 
 setwd('/Users/nsundaresan/Google Drive/College/Autumn 2017/INFO 201/Final-Project-AB3')
 source('scripts/api_keys.R')
 
-response <- GET("https://data.seattle.gov/resource/y7pv-r3kh.json?$limit=5000&$order=date_reported%20DESC", add_headers("X-API-Key" = seattle.app.token))
-body <- content(response, "text")
-data <- fromJSON(body)
-
+GetSPDData <- function() {
+  response <- GET("https://data.seattle.gov/resource/y7pv-r3kh.json?$limit=5000&$order=date_reported%20DESC", add_headers("X-API-Key" = seattle.app.token))
+  body <- content(response, "text")
+  data <- fromJSON(body)
+  return(data)
+}
