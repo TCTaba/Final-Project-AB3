799f295920247d060987cfed0f120b18e5271731
Nanda Sundaresan <sundaresan.nanda@gmail.com>
2017-12-05 15:43:08 -0800

    create plot file

--- scripts/spd_data.R
+++ scripts/spd_data.R
@@ -2,6 +2,7 @@ library(httr)
 library(jsonlite)
 library(dplyr)
 library(plotly)
+library(RColorBrewer)
 
 source('./scripts/api_keys.R')
 
@@ -33,25 +34,6 @@ mis.data <- full.data %>%
 fel.data <- full.data %>%
   filter(summarized_offense_description %in% felonies)
 
-PlotData <- function(df) {
-  p <- df %>%
-    plot_mapbox(lat = ~latitude, lon = ~longitude,
-                split = ~summarized_offense_description,
-                mode = 'scattermapbox', hoverinfo=~date_reported) %>%
-    layout(font = list(color='white'),
-           plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
-           mapbox = list(style = 'dark',
-                         zoom = 10.0,
-                         center = list(lat = median(df$latitude),
-                                       lon = median(df$longitude))),
-           legend = list(orientation = 'v',
-                         font = list(size = 8)),
-           margin = list(l = 0, r = 0,
-                         b = 0, t = 0,
-                         pad = 0))
-  
-  return(p)
-}
 
 
 
