1a812d59bb16e66a39280e69194014f5ec012bd4
timperng <timperng@uw.edu>
2017-12-05 15:18:17 -0800

    make zillow_data.R into function

--- scripts/zillow_data.R
+++ scripts/zillow_data.R
@@ -4,20 +4,30 @@ library(XML)
 
 source('api_keys.R')
 
-url.full <- 'https://www.zillow.com/webservice/GetDeepSearchResults.htm'
-query.params <- list('zws-id' = zillow.api.key, address = '315 Howe St', citystatezip = 'Seattle, WA 98109')
-response <- GET(url.full, query = query.params)
-body <- content(response, 'text')
-results <- xmlToList(body)
+# Find data for specific house, need the address, city, state, and zipcode to search API
+findHouseData <- function(my.address, my.citystatezip){
+  # Access Zillow DeepSearchResults API
+  url.full <- 'https://www.zillow.com/webservice/GetDeepSearchResults.htm'
+  query.params <- list('zws-id' = zillow.api.key, address = my.address, citystatezip = my.citystatezip)
+  response <- GET(url.full, query = query.params)
+  body <- content(response, 'text')
+  # Results are in XML instead of JSON, and xmlToDataFrame doesn't display data correctly
+  results <- xmlToList(body)
+  
+  house.data <- data.frame(address = paste(results$request$address,
+                                           results$request$citystatezip),
+                           price = paste0('$', results$response$results$result$zestimate$amount$text),
+                           bed = results$response$results$result$bedrooms,
+                           bath = results$response$results$result$bathrooms,
+                           year = results$response$results$result$yearBuilt,
+                           sqFt = results$response$results$result$finishedSqFt,
+                           lat = results$response$results$result$address$latitude,
+                           long = results$response$results$result$address$longitude)
+  return(house.data)
+}
 
-house.data <- data.frame(address = paste0(results$response$results$result$address$street, " ",
-                                         results$response$results$result$address$city, ", ",
-                                           results$response$results$result$address$state, " ",
-                                           results$response$results$result$address$zipcode),
-                   price = paste0('$', results$response$results$result$zestimate$amount$text),
-                   bed = results$response$results$result$bedrooms,
-                   bath = results$response$results$result$bathrooms,
-                   year = results$response$results$result$yearBuilt)
+house.data <- findHouseData('356 Galer St', 'Seattle, WA 98109')
+
 
 # url.full.2 <- 'http://www.zillow.com/webservice/GetDeepComps.htm'
 # query.params.2 <- list('zws-id' = zillow.api.key, zpid = results$response$results$result$zpid, count = 25)
