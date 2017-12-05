17d3a45e3ea5e3fa2e3b37bc99f823b041438104
timperng <timperng@uw.edu>
2017-12-05 14:34:28 -0800

    complete draft of zillow_data.R

--- scripts/api_keys.R
+++ scripts/api_keys.R
@@ -1,1 +1,2 @@
 seattle.app.token <- "zPYnBELzMwfAc9FMCtOk7rtXC"
+zillow.api.key <- 'X1-ZWz18wrwx5uvpn_38l6u'
--- scripts/zillow_data.R
+++ scripts/zillow_data.R
@@ -1,7 +1,26 @@
-url.full <- 'http://www.zillow.com/webservice/GetRegionChildren.htm'
-query.params <- list('zws-id' = api.key, state = 'Washington', childtype = 'neighborhood')
+library(jsonlite)
+library(httr)
+library(XML)
+
+source('api_keys.R')
+
+url.full <- 'https://www.zillow.com/webservice/GetDeepSearchResults.htm'
+query.params <- list('zws-id' = zillow.api.key, address = '315 Howe St', citystatezip = 'Seattle, WA 98109')
 response <- GET(url.full, query = query.params)
 body <- content(response, 'text')
 results <- xmlToList(body)
 
-test <- toJSON(results)
+house.data <- data.frame(address = paste0(results$response$results$result$address$street, " ",
+                                         results$response$results$result$address$city, ", ",
+                                           results$response$results$result$address$state, " ",
+                                           results$response$results$result$address$zipcode),
+                   price = paste0('$', results$response$results$result$zestimate$amount$text),
+                   bed = results$response$results$result$bedrooms,
+                   bath = results$response$results$result$bathrooms,
+                   year = results$response$results$result$yearBuilt)
+
+# url.full.2 <- 'http://www.zillow.com/webservice/GetDeepComps.htm'
+# query.params.2 <- list('zws-id' = zillow.api.key, zpid = results$response$results$result$zpid, count = 25)
+# response.2 <- GET(url.full, query = query.params)
+# body.2 <- content(response, 'text')
+# results.2 <- xmlToList(body)
