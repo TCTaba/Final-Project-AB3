32e068004ba9a7d55153c92e7235d8349640a56a
liur17 <31485411+liur17@users.noreply.github.com>
2017-12-05 15:54:38 -0800

    path fixed

---
+++ scripts/neighborhoods.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(httr)
+library(XML)
+source('./scripts/api_keys.R')
+
+url.full <- 'http://www.zillow.com/webservice/GetRegionChildren.htm'
+query.params <- list('zws-id' = zillow.api.key, state = 'Washington', county = 'King', city = 'Seattle', childtype = 'neighborhood')
+response <- GET(url.full, query = query.params)
+body <- content(response, 'text')
+results <- xmlToList(body)
+regions <- results$response$list
+
+SeparateRegions <- function(single.region) {
+  zindex <- single.region$zindex$text
+  if(is.null(zindex)){
+    zindex <- 'NA'
+  } 
+  region.df <- data.frame(single.region$name, single.region$latitude,
+                          single.region$longitude, single.region$url, zindex)
+  colnames(region.df) <- c("Region Name", "Latitude", "Longitude", "URL", "Zindex")
+  return(region.df)
+}
+
+parsed.regions <- SeparateRegions(regions[2]$region) 
+colnames(parsed.regions) <- c("Region Name", "Latitude", "Longitude", "URL", "Zindex")
+
+for(i in 3:103) {
+  parsed.regions <- bind_rows(parsed.regions, SeparateRegions(regions[i]$region))
+}
