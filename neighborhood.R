85bbe632cc1b70bb469e41c3956697de90f43aa7
Rebecca Liu <rebeccaliu@D-108-179-191-224.dhcp4.washington.edu>
2017-12-05 14:58:44 -0800

    neighborhood w/ bug

--- neighborhood.R
+++ neighborhood.R
@@ -1,18 +1,25 @@
-library(jsonlite)
 library(dplyr)
 library(httr)
 library(XML)
 api.key <- 'X1-ZWz18wrwx5uvpn_38l6u'
 
 url.full <- 'http://www.zillow.com/webservice/GetRegionChildren.htm'
-query.params <- list('zws-id' = api.key, state = 'Washington', childtype = 'neighborhood')
+query.params <- list('zws-id' = api.key, city = 'Seattle', childtype = 'neighborhood')
 response <- GET(url.full, query = query.params)
 body <- content(response, 'text')
 results <- xmlToList(body)
 regions <- results$response$list
-count <- 1
-# 106 regions
-print(regions[3])
-#write.csv(results, "xmldata.csv", row.names = FALSE)
 
-#regions <- results$response
+SeparateRegions <- function(single.region) {
+  region.df <- data.frame(single.region$name, single.region$latitude,
+                          single.region$longitude, single.region$url) 
+  colnames(region.df) <- c("Region Name", "Latitude", "Longitude", "URL")
+  return(region.df)
+}
+
+parsed.regions <- SeparateRegions(regions[2]$region) 
+colnames(parsed.regions) <- c("Region Name", "Latitude", "Longitude", "URL")
+
+for(i in 3:107) {
+  parsed.regions <- bind_rows(parsed.regions, SeparateRegions(regions[i]$region))
+}
