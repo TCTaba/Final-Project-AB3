e86e1b1cd870dc39d61e40ff1c2880105ba3fe19
Nanda Sundaresan <sundaresan.nanda@gmail.com>
2017-12-05 18:32:49 -0800

    adds house to map

--- scripts/index.R
+++ scripts/index.R
@@ -5,24 +5,28 @@ source('./scripts/spd_data.R')
 source('./scripts/zillow_data.R')
 source('./scripts/neighborhoods.R')
 
+
 PlotDataAddress <- function(df, address_street, address_CSZ) {
   house.data <- FindHouseData(address_street, address_CSZ)
-  p <- df %>%
-    plot_mapbox(lat = ~latitude, lon = ~longitude,
-                split = ~summarized_offense_description,
-                mode = 'scattermapbox', hoverinfo=~date_reported) %>%
-    layout(font = list(color='white'),
-           autosize=TRUE,
-           plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
-           mapbox = list(style = 'dark',
-                         zoom = 15,
-                         center = list(lat = house.data$lat,
-                                       lon = house.data$long)),
-           legend = list(orientation = 'v',
-                         font = list(size = 8)),
-           margin = list(l = 0, r = 0,
-                         b = 0, t = 0,
-                         pad = 0))
+  p <- plot_mapbox(mode = 'scattermapbox') %>%
+    layout(font = list(color='white'), autosize = TRUE,
+         plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
+         mapbox = list(style = 'dark',
+                       zoom = 15,
+                       center = list(lat = house.data$lat,
+                                     lon = house.data$long)),
+         legend = list(orientation = 'v',
+                       font = list(size = 8)),
+         margin = list(l = 0, r = 0,
+                       b = 0, t = 0,
+                       pad = 0))%>%
+    add_markers(data = df, x = ~longitude, y = ~latitude, text=~summarized_offense_description,
+                hoverinfo = "text", colors=~summarized_offense_description, split=~summarized_offense_description) %>%
+    add_markers(data = house.data, x = ~long, y = ~lat, 
+                text = sprintf("Address: %s,\nPrice: %s,\nBed: %s,\nBath: %s,\nSquare Feet: %s,\n", house.data$address, 
+                               house.data$price, house.data$bed, house.data$bath, house.data$sqft),
+                hoverinfo = "text", marker = list(size="6"))
+  
   return(p)
 }
 
@@ -31,7 +35,7 @@ PlotDataNeighborhood <- function(df, neighborhood) {
   p <- df %>%
     plot_mapbox(lat = ~latitude, lon = ~longitude,
                 split = ~summarized_offense_description,
-                mode = 'scattermapbox', hoverinfo=~date_reported) %>%
+                mode = 'scattermapbox', text=~summarized_offense_description, hoverinfo = "text") %>%
     layout(font = list(color='white'),
            plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
            autosize = TRUE, 
@@ -44,5 +48,9 @@ PlotDataNeighborhood <- function(df, neighborhood) {
            margin = list(l = 0, r = 0,
                          b = 0, t = 0,
                          pad = 0))
+
   return(p)
 }
+
+PlotDataAddress(full.data, '315 Howe Street', 'Seattle WA, 98109')
+#PlotDataNeighborhood(full.data, "University District")
